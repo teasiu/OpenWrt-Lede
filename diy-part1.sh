32eda474057b59e894e37366c5c3664bbc49e941
teasiu <teasiu@qq.com>
2021-03-09 14:30:30 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/teasiu/lede-other-apps.git package/lede-other-apps
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lede-other-apps/luci-app-argon-config
 wget https://github.com/ButterAndButterfly/GithubHost/releases/download/v1/host.txt -O package/base-files/files/etc/githubhosts
 bash -c "cat >> package/network/services/dnsmasq/files/dnsmasq.conf" << EOF
 enable-tftp
