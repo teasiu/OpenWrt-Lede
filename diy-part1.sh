14916a2361bfa8986d3c05f6c0fcba3be49e1111
teasiu <teasiu@qq.com>
2021-03-09 08:31:59 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,15 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/teasiu/lede-other-apps.git package/lede-other-apps
 wget https://raw.githubusercontent.com/teasiu/OpenWrt-Newifi_D2/main/uhttpd -O package/network/services/uhttpd/files/uhttpd.config
+wget https://github.com/ButterAndButterfly/GithubHost/releases/download/v1/host.txt -O package/base-files/files/etc/githubhosts
+bash -c "cat >> package/network/services/dnsmasq/files/dnsmasq.conf" << EOF
+enable-tftp
+tftp-root=/www/tftp
+dhcp-boot=undionly.kpxe
+dhcp-match=set:efi-x86_64,option:client-arch,7
+dhcp-boot=tag:efi-x86_64,uefi/snponly.efi
+dhcp-hostsfile=/etc/githubhosts
+EOF
 #sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
 
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
